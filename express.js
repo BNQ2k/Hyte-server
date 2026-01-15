85a03c1d5b9edd346cbe0f42a4a68749fb61310e
BNQ2k <benjamin.lepisto@metropolia.fi>
2026-01-15 11:15:04 +0200

    commit

--- express.js
+++ express.js
@@ -1,11 +1,22 @@
 import express from 'express';
+const hostname = '127.0.0.1';
 const app = express();
 const port = 3000;
 
+const items = [
+  { "id": 1, "name": "kivi" },
+  { "id": 2, "name": "bisse" },
+  { "id": 3, "name": "makkara" }
+];
+
 app.get('/', (req, res) => {
-  res.send('Tämä on Express-palvelin!');
+  res.send('Welcome to my REST API!');
 });
 
-app.listen(port, () => {
-  console.log(`Express-palvelin pyörii osoitteessa http://localhost:${port}`);
+app.get('/items', (req, res) => {
+  res.json(items);
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Express-palvelin pyörii osoitteessa http://${hostname}:${port}`);
 });
